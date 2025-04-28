857d9170221cc6e87284ac9b81928f3f059d55b0
SebastianGaleano20 <sebascarp9@gmail.com>
2025-04-28 11:04:02 -0300

    Models for Guest created

--- src/models/guestModel.ts
+++ src/models/guestModel.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../config/prisma";
+import { Guest } from "../types";
 
 export const GuestModel = () => {
   // Modelo para encontrar Invitado por token
@@ -9,7 +10,29 @@ export const GuestModel = () => {
       },
     });
   };
+  const confirmAttendance = (token: string, data: Guest) => {
+    return prisma.guest.update({
+      where: {
+        token,
+      },
+      data: {
+        ...data,
+        status: "CONFIRMED",
+      },
+    });
+  };
+  const createGuest = (data: Guest) => {
+    return prisma.guest.create({
+      data,
+    });
+  };
+  const getAllGuest = () => {
+    return prisma.guest.findMany();
+  };
   return {
     findByToken,
+    confirmAttendance,
+    createGuest,
+    getAllGuest,
   };
 };
