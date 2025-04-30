0d45f22a7ae3f425f878b83498fadfd89b8dc8e6
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2025-04-30 13:17:48 +0000

    guestController created

---
+++ src/controllers/guestController.ts
@@ -0,0 +1,32 @@
+import { Response, Request, NextFunction } from "express";
+import { GuestService } from "../services/guestService";
+import httpStatus from "../helpers/httpStatus";
+
+const { validateGuest, confirmGuest } = GuestService();
+export const GuestController = () => {
+    const validate = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        const { name, token } = req.body;
+        const guest = await validateGuest(name, token);
+        if (!guest) return res.status(httpStatus.UNAUTHORIZED).json({ message: "Unauthorized" });
+        next();
+    };
+    const confirm = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        const { token } = req.params;
+        const guest = await confirmGuest(token, req.body);
+        if (!guest) return res.status(httpStatus.UNAUTHORIZED).json({ message: "Unauthorized" });
+        next();
+    };
+
+    return {
+        validate,
+        confirm
+    }
+};
