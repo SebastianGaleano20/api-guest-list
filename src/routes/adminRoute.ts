80488910b629e4581ec5d68f370eec3abea61b9b
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2025-05-07 12:06:29 +0000

    adminRoute - Routes created

---
+++ src/routes/adminRoute.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { AdminController } from "../controllers/adminController";
+
+export const adminRoutes = () => {
+    const adminRouter = Router();
+    const { loginAdmin, getAllGuest, deleteGuest, getGuestById, createGuest, updateGuest } = AdminController();
+    adminRouter.route("/login").post(loginAdmin);
+    adminRouter.route("/guests")
+        .post(createGuest)
+        .get(getAllGuest)
+    adminRouter.route("/guests/:id")
+        .delete(deleteGuest)
+        .get(getGuestById)
+        .patch(updateGuest);
+}
