85e086c52f43af77012b3d16a1eb1fd66249752b
SebastianGaleano20 <sebascarp9@gmail.com>
2025-09-08 14:42:18 -0300

    fix ( index types ) - delete and update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,12 @@
 type Status = "PENDING" | "CONFIRMATED" | "CANCELED";
 
-export type ConfirmedGuest = { name: string; confirmed: boolean };
 export interface Guest {
-  id?: number;
   firstName: string;
   lastName: string;
-  email: string;
-  phone: string;
-  image: string;
   status: Status;
   token: string;
-  confirmedGuests: ConfirmedGuest[];
-  musicSuggestion: string[];
+  confirmedGuests: string;
+  musicSuggestion: string;
 }
 export interface Admin {
   name: string;
