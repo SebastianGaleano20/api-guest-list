2493b5337a974bd4ab8d586480a06b3076d05f52
SebastianGaleano20 <sebascarp9@gmail.com>
2025-09-08 15:45:12 -0300

    feat (guestSchema) - add updateGuestSchema

--- src/schemas/guestSchema.ts
+++ src/schemas/guestSchema.ts
@@ -4,9 +4,18 @@ export const guestSchema = Joi.object({
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
   token: Joi.string(),
-  status: Joi.string(),
+  status: Joi.string().valid("PENDING", "CONFIRMED", "DECLINED").optional(),
   confirmedGuests: Joi.array().optional,
   musicSuggestion: Joi.array().optional,
+});
+
+export const updateGuestSchema = Joi.object({
+  firstName: Joi.string().optional(),
+  lastName: Joi.string().optional(),
+  token: Joi.string().optional(),
+  status: Joi.string().valid("PENDING", "CONFIRMED", "DECLINED").optional(),
+  confirmedGuests: Joi.string().optional(),
+  musicSuggestion: Joi.string().optional(),
 });
 
 export const guestValidate = Joi.object({
